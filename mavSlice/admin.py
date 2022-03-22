a2b3a4c584042339b630aac66cdb9d19c6b22c0e
bcash10 <80350534+bcash10@users.noreply.github.com>
2022-03-22 14:45:02 -0500

    requirements.txt changed

--- mavSlice/admin.py
+++ mavSlice/admin.py
@@ -7,10 +7,10 @@ from .models import *
 # Should be able to see name, email, Delivery information, user_id
 # Delivery info includes: address,
 class CustomerList(admin.ModelAdmin):
-    #list_display = ('cust_id',)
-    list_display = ('cust_id', 'user.email', 'user.first_name', 'user.last_name', 'delivery_info.street_address',
-                    'delivery_info.street_address2', 'delivery_info.city', 'delivery_info.state',
-                    'delivery_info.zipCode')
+    list_display = ('cust_id',)
+    #list_display = ('cust_id', 'user.email', 'user.first_name', 'user.last_name', 'delivery_info.street_address',
+    #                'delivery_info.street_address2', 'delivery_info.city', 'delivery_info.state',
+    #                'delivery_info.zipCode')
     list_filter = ('user.last_name', 'cust_id',)
     #list_filter = ('cust_id',)
     search_fields = ('cust_id', 'user.first_name', 'user.last_name', 'user.email', 'delivery_info.zipCode')
