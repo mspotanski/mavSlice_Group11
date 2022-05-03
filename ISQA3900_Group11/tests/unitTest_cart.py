0980622d1c1de4799b7b634c79a50b5a5c536bb1
kkandel940 <98285659+kkandel940@users.noreply.github.com>
2022-05-03 14:05:53 -0500

    k

---
+++ ISQA3900_Group11/tests/unitTest_cart.py
@@ -0,0 +1,32 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+import warnings
+
+class ll_ATS(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Safari()
+        warnings.simplefilter('ignore', ResourceWarning)
+
+        def test_ll(self):
+            driver = self.driver
+            driver.maximize_window()
+            driver.get("http://127.0.0.1:800")
+            time.sleep(10)
+                # look for Cart in navbar
+            elem= driver.find_element(By.XPATH,'//*[@id="myNavbar"]/ul[1]/li[4]/a').click()
+            time.sleep(8)
+
+            try:  #look for Continue shopping now buttom
+                elem = driver.find_element(By.XPATH,'//*[@id="app-layout"]/div/div[2]/div/a[1]')
+                print("Test passed- Cart page displayed")
+                assert True
+            except NoSuchElementException:
+                self.fail("Cart page does not apper- test failed")
+def tearDown(self):
+    self.driver.close()
+if __name__ == "__main__":
+    unittest.main()
